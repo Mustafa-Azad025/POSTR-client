5ee5a8837dd9236b99fce764fa8695bb596b4f67
Mustafa-Azad <mustafaazad533@gmail.com>
2023-02-17 20:44:54 +0530

    Credential Auth

---
+++ pages/api/auth/signup.ts
@@ -0,0 +1,29 @@
+import connectDB from "../../../database/connect";
+import Users from "../../../model/registerUser";
+import { hash } from "bcryptjs";
+export default async function handler(req: any, res: any) {
+	connectDB().catch(() => res.json({ error: "Connection Failed . . . " }));
+
+	if (req.method === "POST") {
+		if (!req.body) {
+			return res.status(404).json({ error: "Don't Have Form Data" });
+		}
+		const { fullname, email, password, image } = req.body;
+
+		// checking dublicate users
+		const check = await Users.findOne({ email });
+		if (check) return res.status(422).json({ message: "User Already Exists" });
+
+		Users.create(
+			{ fullname, email, password: await hash(password, 12), image },
+			function (err: any, data: any) {
+				if (err) return res.status(404).json({ err });
+				res.status(201).json({ status: true, user: data });
+			}
+		);
+	} else {
+		res
+			.status(500)
+			.json({ message: "HTTP request is not valid only post accepted" });
+	}
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+		"./layout/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {},
+		colors: {
+			primary: "#00ADB5",
+			light: "#EEEEEE",
+			mid: "#393E46",
+			warn: "#ff0000",
+			dark: "#222831",
+			white: "#fff",
+			secondary: "#9DFFFE",
+		},
+	},
+	plugins: [require("tailwind-scrollbar-hide")],
+};
